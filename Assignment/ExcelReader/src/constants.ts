0afac9a3dc4aa1bb53108f4c699b72e5c7e37312
S.M.A Sayem Prodhan <ananta.ap69@gmail.com>
2024-10-17 13:34:34 +0600

    update endpoints

--- Assignment/ExcelReader/src/constants.ts
+++ Assignment/ExcelReader/src/constants.ts
@@ -1,11 +1,11 @@
-export const ApiBaseUrl = 'https://192.168.100.52:5150/api';
-export const ApiBaseImageUrl = 'https://192.168.100.52:5150';
-export const ApiRealtimeUrl = 'https://192.168.100.52:5150/Realtime';
+// export const ApiBaseUrl = 'https://192.168.100.52:5150/api';
+// export const ApiBaseImageUrl = 'https://192.168.100.52:5150';
+// export const ApiRealtimeUrl = 'https://192.168.100.52:5150/Realtime';
 
-// export const ApiBaseUrl = 'https://regular-impala-safely.ngrok-free.app/api';
-// export const ApiBaseImageUrl = 'https://regular-impala-safely.ngrok-free.app';
-// export const ApiRealtimeUrl =
-//   'https://regular-impala-safely.ngrok-free.app/Realtime';
+export const ApiBaseUrl = 'https://regular-impala-safely.ngrok-free.app/api';
+export const ApiBaseImageUrl = 'https://regular-impala-safely.ngrok-free.app';
+export const ApiRealtimeUrl =
+  'https://regular-impala-safely.ngrok-free.app/Realtime';
 
 export const GoogleClientId =
   '590226343591-eriqfsv9jrc29v5q85bvcvsapt7gspre.apps.googleusercontent.com';
