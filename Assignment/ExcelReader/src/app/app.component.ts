efd312ac3da687a90364187622922221704a57a5
S.M.A Sayem Prodhan <ananta.ap69@gmail.com>
2024-10-29 14:53:42 +0600

    update navigation handling mechanism

--- Assignment/ExcelReader/src/app/app.component.ts
+++ Assignment/ExcelReader/src/app/app.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationSkipped,
+  NavigationStart,
+  Router,
+  RouterLink,
+  RouterOutlet,
+} from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  MediaMatcher,
+} from '@angular/cdk/layout';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import { map } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgIf } from '@angular/common';
-import { HomeComponent } from "./dashboard/home/home.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
+import { HomeComponent } from './dashboard/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { AuthService } from './services/auth.service';
 import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { ToastrService } from 'ngx-toastr';
-import { ChatComponent } from "./dashboard/chat/chat.component";
+import { ChatComponent } from './dashboard/chat/chat.component';
 import { RealtimeService } from './services/realtime.service';
-import { UserListComponent } from "./dashboard/chat/user-list/user-list.component";
+import { UserListComponent } from './dashboard/chat/user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
@@ -38,20 +49,20 @@ import { UserListComponent } from "./dashboard/chat/user-list/user-list.componen
     LoginComponent,
     SignupComponent,
     ChatComponent,
-    UserListComponent
+    UserListComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit, OnDestroy {
   title = 'FileKeeper';
 
-  isHandset$ = false
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isHandset$ = false;
 
-  isAuthenticated = this.authService.isAuthenticated
-  isAdmin = this.authService.isAdmin
-
+  isAuthenticated = this.authService.isAuthenticated;
+  isAdmin = this.authService.isAdmin;
+  navigationSubscription!: Subscription;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -61,42 +72,68 @@ export class AppComponent implements OnInit {
     private toastrService: ToastrService,
     private realtimeService: RealtimeService
   ) {
-    this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-      map(result => this.isHandset$ = result.matches)
-    );
+    this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .pipe(map((result) => (this.isHandset$ = result.matches)));
   }
 
   ngOnInit(): void {
-    this.socialAuthService.authState.subscribe((user) => {
-      //social login
-      this.onSocialAuth(user.idToken)
-    })
-
+    this._navigationAutoClose();
+    this._socialServiceAuth();
     //application wide realtime comm.
-    this.realtimeService.startConnection();
-    this.realtimeService.addReceiveMessageListener<string[]>('ReceiveMessage', (message) => {
-      this.toastrService.show("Realtime message: " + message);
-    });
-
-
+    this._realtimeComms();
   }
 
   logout() {
-    this.authService.logout()
-    this.router.navigate(['/'])
-    window.location.reload()
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
-
 
   onSocialAuth(idToen: string) {
     this.authService.socialAuth(idToen).subscribe({
       next: (response) => {
         //login ok
-        this.toastrService.success(response.message)
-        this.router.navigate(['/'])
-      }
-    })
+        this.toastrService.success(response.message);
+        this.router.navigate(['/']);
+      },
+    });
   }
 
+  ngOnDestroy(): void {
+    this.navigationSubscription.unsubscribe();
+  }
 
+  ///////private function to declutter oninit/////
+  private _navigationAutoClose() {
+    this.navigationSubscription = this.router.events.subscribe((event) => {
+      console.log(event);
+      if (
+        event instanceof NavigationStart ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationSkipped
+      ) {
+        if (this.sidenav.opened) {
+          //close
+          this.sidenav.close();
+        }
+      }
+    });
+  }
+
+  private _socialServiceAuth() {
+    this.socialAuthService.authState.subscribe((user) => {
+      //social login
+      this.onSocialAuth(user.idToken);
+    });
+  }
+
+  private _realtimeComms() {
+    this.realtimeService.startConnection();
+    this.realtimeService.addReceiveMessageListener<string[]>(
+      'ReceiveMessage',
+      (message) => {
+        this.toastrService.show('Realtime message: ' + message);
+      }
+    );
+  }
 }
